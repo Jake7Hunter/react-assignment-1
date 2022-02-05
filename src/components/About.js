79a413f1a0912bbd039dd08b673abe844c76076a
Jake Hunter <jake7hunter@hmail.com>
2022-02-04 19:03:07 -0500

    Added about page

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Info extends React.Component {
+  render() {
+    return(
+        <article className="middle">
+          <h2>Gallery</h2>
+        </article>
+    )
+  }
+}
+
+export default Info
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/Header';
+import About from './components/About';
 import Gallery from './components/Gallery';
 import Footer from './components/Footer';
 import './css/style.css';
@@ -8,6 +9,7 @@ import './css/style.css';
 ReactDOM.render(
   <React.StrictMode>
     <Header />
+    <About />
     <Gallery />
     <Footer />
   </React.StrictMode>,
